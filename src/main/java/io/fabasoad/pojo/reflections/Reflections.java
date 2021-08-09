1bfa5ff628156454dbcb8f68e52d2fc617b90625
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2021-08-09 11:06:18 +0900

    Add recursive check (#2)

--- src/main/java/io/fabasoad/pojo/reflections/Reflections.java
+++ src/main/java/io/fabasoad/pojo/reflections/Reflections.java
@@ -7,21 +7,25 @@ import com.google.common.reflect.ClassPath.ClassInfo;
 import io.fabasoad.pojo.reflections.exceptions.ReflectionsException;
 import java.io.IOException;
 import java.util.Set;
+import java.util.function.Predicate;
 
 public class Reflections {
 
   @SuppressWarnings("All")
-  public Set<Class<?>> getClasses(final String packageName) {
+  public Set<Class<?>> getClasses(final String packageName, final boolean recursive) {
     final ClassPath classPath;
     try {
       classPath = ClassPath.from(ClassLoader.getSystemClassLoader());
     } catch (IOException e) {
       throw new ReflectionsException("Failed to read class path", e);
     }
+    final Predicate<ClassInfo> filter = recursive
+        ? c -> c.getPackageName().startsWith(packageName)
+        : c -> c.getPackageName().equalsIgnoreCase(packageName);
     return classPath
         .getAllClasses()
         .stream()
-        .filter(c -> c.getPackageName().equalsIgnoreCase(packageName))
+        .filter(filter)
         .map(ClassInfo::load)
         .collect(toSet());
   }
--- src/test/groovy/io/fabasoad/pojo/PojoValidatorSpec.groovy
+++ src/test/groovy/io/fabasoad/pojo/PojoValidatorSpec.groovy
@@ -1,26 +1,50 @@
 package io.fabasoad.pojo
 
-import io.fabasoad.pojo.validator.fixture.getters.B
-import io.fabasoad.pojo.validator.fixture.getters.A
 import io.fabasoad.pojo.validator.PojoValidatorBuilder
+import io.fabasoad.pojo.validator.fixture.fields.A
+import io.fabasoad.pojo.validator.fixture.fields.B
+import io.fabasoad.pojo.validator.fixture.fields.C
+import io.fabasoad.pojo.validator.fixture.fields.D
+import io.fabasoad.pojo.validator.fixture.fields.E
 import spock.lang.Specification
 
 class PojoValidatorSpec extends Specification {
 
-  def PACKAGE_NAME = "io.fabasoad.pojo.validator.fixture.getters"
-
-  def "Get correct list of classes"() {
+  def "Get correct list of classes with default settings"() {
     given:
-    def builder = PojoValidatorBuilder.create(PACKAGE_NAME)
+    def builder = PojoValidatorBuilder.create(packageName)
 
     when:
     def validator = builder.build()
     def actual = new ArrayList<Class<?>>(validator.classes())
-    def expected = [A, B]
 
     then:
-    2 == validator.classes().size()
+    expected.size() == validator.classes().size()
     0 == validator.testers().size()
     [] == ((expected - actual) + (actual - expected))
+
+    where:
+    packageName                                 | expected
+    "io.fabasoad.pojo.validator.fixture"        | []
+    "io.fabasoad.pojo.validator.fixture.fields" | [A, B, C, D, E]
+  }
+
+  def "Get correct list of classes"() {
+    given:
+    def builder = PojoValidatorBuilder.create(packageName, recursive)
+
+    when:
+    def validator = builder.build()
+    def actual = new ArrayList<Class<?>>(validator.classes())
+
+    then:
+    expected.size() == validator.classes().size()
+    0 == validator.testers().size()
+    [] == ((expected - actual) + (actual - expected))
+
+    where:
+    recursive | packageName                                 | expected
+    true      | "io.fabasoad.pojo.validator.fixture"        | [io.fabasoad.pojo.validator.fixture.getters.A, io.fabasoad.pojo.validator.fixture.getters.B, A, B, C, D, E]
+    false     | "io.fabasoad.pojo.validator.fixture.fields" | [A, B, C, D, E]
   }
 }
