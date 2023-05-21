443668cad3f97779f8644a03819d1422824ab625
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2023-05-21 14:05:57 +0900

    Fix make dev/install-tools command

--- tasks/Makefile.dev.mk
+++ tasks/Makefile.dev.mk
@@ -5,7 +5,7 @@ dev/install: dev/install-tools dev/install-deps ## Installs all the required too
 dev/install-tools: ## Installs all the tools that are required for the correct work with this project using 'asdf' tool. All tools and their versions are listed in '.tool-versions' file
 	@asdf plugin list | grep -q java || asdf plugin add java
 	@asdf plugin list | grep -q pre-commit || asdf plugin add pre-commit
-	@asdf install
+	@while IFS= read -r l; do asdf install $(echo "$l" | cut -d' ' -f1) $(echo "$l" | cut -d' ' -f2); done < .tool-versions
 
 dev/install-deps: ## Installs all the dependencies that are required for the correct work of the tools listed in '.tool-versions' file
 	@pre-commit install
