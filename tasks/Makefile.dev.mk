08bff5c4e2e167294c71afedb8225e3d3de34a02
Eugene <fabasoad@gmail.com>
2025-01-23 15:05:52 +0900

    Bump gitleaks/gitleaks from 8.22.1 to 8.23.1 (#83)
    ## Description
    
    - Bump `gitleaks/gitleaks` from `8.22.1` to `8.23.1`
    - Add `fabasoad/pre-commit-checkstyle` hook
    - Bump `org.spockframework:spock-core` from `2.4-M4-groovy-4.0` to
    `2.4-M5-groovy-4.0`
    - Bump `asdf` tools to the latest version
    - Add `--refresh-dependencies --warning-mode all` to `make java/build`

--- tasks/Makefile.dev.mk
+++ tasks/Makefile.dev.mk
@@ -5,8 +5,10 @@ dev/install: dev/install-tools dev/install-deps ## Installs all the required too
 dev/install-tools: ## Installs all the tools that are required for the correct work with this project using 'asdf' tool. All tools and their versions are listed in '.tool-versions' file
 	@asdf plugin list | grep -q java || asdf plugin add java
 	@asdf plugin list | grep -q pre-commit || asdf plugin add pre-commit
+	@asdf plugin list | grep -q coursier || asdf plugin add coursier
 	@asdf install java $(grep -Eo "java [a-z0-9-]+" ".tool-versions" | cut -d ' ' -f 2)
 	@asdf install pre-commit $(grep -Eo "pre-commit [0-9.pre-]+" ".tool-versions" | cut -d ' ' -f 2)
+	@asdf install coursier $(grep -Eo "coursier [0-9.pre-]+" ".tool-versions" | cut -d ' ' -f 2)
 
 dev/install-deps: ## Installs all the dependencies that are required for the correct work of the tools listed in '.tool-versions' file
 	@pre-commit install
--- tasks/Makefile.java.mk
+++ tasks/Makefile.java.mk
@@ -1,7 +1,7 @@
 .PHONY: java/build java/global java/test java/update
 
 java/build: ## Builds the whole project using gradle
-	@./gradlew build
+	@./gradlew build --refresh-dependencies --warning-mode all
 
 java/global: ## Sets Java version of this project as a global Java version
 	@grep -Eo 'java [0-9.-]+' .tool-versions | xargs asdf global
